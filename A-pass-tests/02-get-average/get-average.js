3707f9c24988ea1d69abd7eae05a207bf9d0a236
simeonbikov <bikovsem@gmail.com>
2022-11-08 17:35:38 +0000

    part A completed

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,12 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+    let arrOfNum = numbers.filter(value => typeof value === 'number');
+    let sum = arrOfNum.reduce((acc, n) => acc + n);
+    return sum / arrOfNum.length;
+}
 
 module.exports = average;
+
+// console.log(average([13, 10, 12, "i", "a"]));
--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,13 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  const totals = {};
+  carsSold.forEach((carSale) => {
+    if (totals[carSale.make]) {
+      totals[carSale.make] += carSale.price;
+    } else {
+      totals[carSale.make] = carSale.price;
+    }
+  });
+  return totals;
+}
 
 module.exports = sales;
